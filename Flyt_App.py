18c4acddc125333888fcef22936ce606e732e9f3
GAURAV KUMAR <gaurav656ae@gmail.com>
2023-06-21 12:42:31 +0530

    Update Flyt_App.py

--- Flyt_App.py
+++ Flyt_App.py
@@ -40,9 +40,9 @@ print ('taking off')
 drone.take_off(10.0)
 
 print ('going along the setpoints')
-drone.position_set(0,10,0,relative=True)
-drone.position_set(8.66,-5,0,relative=True)
-drone.position_set(-8.66,-5,0,relative=True)
+drone.position_set(8.66,5,0,relative=True)
+drone.position_set(-8.66,5,0,relative=True)
+drone.position_set(0,-10,0,relative=True)
 
 print ('Landing')
 drone.land(async=False)
